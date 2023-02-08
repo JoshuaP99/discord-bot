d1d995d9328a7ccb0d10c02c1c2feb779bcd2895
Joshua <pikminguy56@gmail.com>
2023-02-07 18:05:42 -0700

    Newest update

--- app.js
+++ app.js
@@ -1,29 +1,71 @@
-const { Client, GatewayIntentBits } = require('discord.js')
-require('dotenv/config')
+const Discord = require('discord.js')
+const dotenv = require("dotenv")
+const { REST } = require("@discordjs/rest")
+const { Routes } = require("@discord-api-types/v9")
+const fs = require("fs")
+const { Player } = require("discord-player")
+
+dotenv.config()
+const TOKEN = process.env.TOKEN
+
+const LOAD_SLASH = process.argv[2] == "load"
+
+const CLIENT_ID = 
+const GUILD_ID = 
 
 const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ],
+        "GUILDS",
+        "GUILDS_VOICE_STATES"
+    ]
 })
 
-const prefix = '~'
-
-client.on('ready', () => {
-    console.log('Bot loaded')
-})
-
-client.on ('messageCreate', message => {
-    if (message.content.startsWith(prefix) || message.author.bot) {
-        const args = message.content.slice(prefix.length).split(/ +/)
-        const command = args.shift().toLowerCase();
-
-        if (command === 'youtube') {
-            message.channel.send('https://youtu.be/X1FC7qjBufk')
-        }
+client.slashcommands = new Discord.Collection()
+client.player = new Player(client, {
+    ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25
     }
 })
 
-client.login(process.env.TOKEN)
+let commands = []
+
+const slashFiles = fs.readdir("./slash").filter(file => file.endsWith(".js"))
+for (const file of slashFiles){
+    const slashcmd = require(`./slash/${file}`)
+    client.slashcommands.set(slashcmd.data.name, slashcmd)
+    if (LOAD_SLASH) commands.push(slashcmd.data.toJSON())
+}
+
+if (LOAD_SLASH) {
+    const rest = new REST ({ version: "9" }).setToken(TOKEN)
+    console.log("Deploying slash commands")
+    rest,put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {body: commands})
+    .then(() => {
+        console.log("Loaded Succesfully")
+        process.exit(0)
+    })
+    .catch((err) => {
+        if (err) {
+            console.log(err)
+            process.exit(1)
+        }
+    })
+} else {
+    client.on("ready", () => {
+        console.log(`logged in as ${client.user.tag}`)
+    })
+    client.on("interactionCreate", (interaction) => {
+        async function handleCommand(){
+            if (!interaction.isCommand()) return
+
+            const slashcmd = client.slashcommands.get(interaction.commandName)
+            if (!slashcmd) interaction.reply("Not a slash command")
+
+            await interaction.deferReply()
+            await slashcmd.run({ client, interaction })
+        }
+        handleCommand()
+    })
+    client.login(TOKEN)
+}
